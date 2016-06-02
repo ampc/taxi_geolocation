e21d8c752c720cd46ceeb0cc2a5432fed58c8571
António Carvalho <antoniocarvalho800@gmail.com>
2016-06-02 03:17:01 +0100

    server v2

--- server/tabd/routes/index.js
+++ server/tabd/routes/index.js
@@ -18,7 +18,7 @@ router.get('/api/paths/:id_car',function (req,res) {
 			return res.status(500).json({success:false, data:err});
 		}
 
-		var query = client.query("SELECT id_car, ST_AsGeoJSON(geom) FROM paths WHERE id_car=($1)",[id]);
+		var query = client.query("SELECT id_car, ST_AsText(geom) FROM paths WHERE id_car=($1)",[id]);
 
 		query.on('row',function (row) {
 			results.push(row);
@@ -32,6 +32,7 @@ router.get('/api/paths/:id_car',function (req,res) {
 
 	})
 });
+
 router.get('/api/paths/timestamps/:id_car',function (req,res) {
 	var results =[];
 	var id = req.params.id_car;
@@ -56,6 +57,29 @@ router.get('/api/paths/timestamps/:id_car',function (req,res) {
 
 	})
 });
+router.get('/api/paths/',function (req,res) {
+	var results =[];
+	pg.connect(connectionString, function (err,client,done) {
+		if(err){
+			done();
+			console.log(err);
+			return res.status(500).json({success:false, data:err});
+		}
+
+		var query = client.query("SELECT id_car FROM paths;");
+
+		query.on('row',function (row) {
+			results.push(row);
+
+		});
+
+		query.on('end',function(row){
+			done();
+			return res.json(results);
+		});
+
+	})
+});
 router.get('/api/posturas/',function (req,res) {
 	var results =[];
 	pg.connect(connectionString, function (err,client,done) {
@@ -65,7 +89,7 @@ router.get('/api/posturas/',function (req,res) {
 			return res.status(500).json({success:false, data:err});
 		}
 
-		var query = client.query("SELECT name, ST_AsGeoJSON(local_post) FROM posturas;");
+		var query = client.query("SELECT name, ST_AsText(local_post)  FROM posturas;");
 
 		query.on('row',function (row) {
 			results.push(row);
@@ -90,8 +114,34 @@ router.get('/api/posturas/:id_post',function (req,res) {
 			return res.status(500).json({success:false, data:err});
 		}
 
-		var query = client.query("SELECT name, ST_AsGeoJSON(local_post), ST_AsGeoJSON(lugares_post) FROM posturas WHERE id_post=($1);",[id_post]);
+		var query = client.query("SELECT name, ST_AsText(local_post) as local_post, ST_AsText(lugares_post) as lugares_post FROM posturas WHERE id_post=($1);",[id_post]);
 
+		query.on('row',function (row) {
+			results.push(row);
+
+		});
+
+		query.on('end',function(row){
+			done();
+			return res.json(results);
+		});
+
+	})
+});
+
+
+router.get('/api/nearest/:coords',function (req,res) {
+	var results =[];
+	var coords = req.params.coords;
+	pg.connect(connectionString, function (err,client,done) {
+		if(err){
+			done();
+			console.log(err);
+			return res.status(500).json({success:false, data:err});
+		}
+
+		var query = client.query("SELECT name, st_AsText(local_post), st_distance(local_post, dcc)/1000 AS distance_km FROM posturas, (select ST_MakePoint("+coords+")::geography as dcc) as dcc WHERE ST_DWithin(local_post, dcc, 10000) ORDER BY ST_Distance(local_post, dcc) LIMIT 10;");
+		console.log(query);
 		query.on('row',function (row) {
 			results.push(row);
 
